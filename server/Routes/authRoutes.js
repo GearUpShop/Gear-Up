d8069308126b2d058b2cff7f5fd3a4c960ff4436
rafatmaita <2030684@std.hu.edu.jo>
2023-12-05 12:55:37 +0300

    rafat

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require('../controller/authController');
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/users', authController.getAllUsers);
+router.delete('/user/:userId', authController.softDeleteUser);
 
 module.exports = router;
